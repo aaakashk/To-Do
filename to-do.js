f6a35e5609107c314a7a6907e7a9a674896800c0
Akash Kumar <akashyadav112314@gmail.com>
2022-08-21 02:51:08 +0530

    can't make the input box to work as intended

--- to-do.js
+++ to-do.js
@@ -1,8 +1,18 @@
-const text = document.querySelector("input");
+const inputTag = document.querySelector("input");
 const btn = document.querySelector("button");
-const para = document.querySelector("p");
-function getValue() {
-    para.innerHTML = text.value;
+const mainpage = document.querySelector(".mainpage");
+function addToDo() {
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.setAttribute("id", "to-do-1");
+    const label = document.createElement("label");
+    label.setAttribute("for", "to-do-1");
+    label.innerText = inputTag.value;
+    input.append(label);
+    mainpage.append(input);
+    inputTag.value = "";
+    // mainpage.classList.add("new-to-do");
+    // mainpage.append(div);
 }
 
-btn.addEventListener("click", getValue);
+btn.addEventListener("click", addToDo);
